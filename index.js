904ebc5596d8905375aba144181354e5eb380c34
Shrey Raj <shreyraj1653@gmail.com>
2025-05-22 17:00:03 +0530

    PORT 3000

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { exec } = require('child_process');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = 80;
+const PORT = 3000;
 
 const VPS_IP = '65.20.77.245';
 
